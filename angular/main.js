b1ab3e33a8502d0694770a7261ba7309753f4b1c
bannerss <roey.kanner@gmail.com>
2022-06-16 04:20:54 +0300

    dfg

--- angular/main.js
+++ angular/main.js
@@ -1310,7 +1310,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    apiUrl: "http://localhost:3000/api"
+    apiUrl: "https://modspellcheck.azurewebsites.net/api"
 };
 /*
  * In development mode, to ignore zone related error stack frames such as
