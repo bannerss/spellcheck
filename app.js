990a6e461a86dfd2b2991efd22e19ca06bf6974c
bannerss <roey.kanner@gmail.com>
2022-06-16 11:55:21 +0300

    dfg

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ mongoose
       "@cluster0-ntrwp.mongodb.net/node-angular"
   )
   .then(() => {
-    console.log("Connected to database!");
+    console.log("Connected to mongoose database!");
   })
   .catch(() => {
-    console.log("Connection failed!");
+    console.log("mongoose Connection failed!");
   });
 
 app.use(bodyParser.json());
@@ -26,6 +26,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/", express.static(path.join(__dirname, "angular")));
 
+app.use(function(req, res, next) {
+  console.log = function() {}
+    if (req.hostname === 'izkor.mod.gov.il' || req.hostname === 'izkor.gov.il' || req.hostname === 'www.izkor.mod.gov.il') {
+      return res.redirect(301, 'https://www.izkor.gov.il/' + req.originalUrl);
+    }
+    /*if(req.hostname.indexOf('https://www.izkor.gov.il') < 0 && req.query.utm_source)
+    {
+      return res.redirect(301, 'https://www.izkor.gov.il/' + req.originalUrl);
+    }*/
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('X-Frame-Options', 'sameorigin');  // deny
+    res.setHeader('X-Content-Type-Options', 'nosniff');  //disable nosniff? for MIME mismatch?
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+    //res.setHeader('Strict-Transport-Security', 'max-age=31536000');
+
+    //res.setHeader('Cache-Control', 'no-cache');
+
+
+    //handle cors:
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, MERGE, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', "true");
+    next();
+});
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader(
@@ -42,6 +70,7 @@ app.use("/", express.static(path.join(__dirname, "angular")));
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
 app.use((req, res, next) => {
+  console.log("index.html");
   res.sendFile(path.join(__dirname, "angular", "index.html"));
 });
 
