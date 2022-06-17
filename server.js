c4203ad87a3874fe46d0d655ecf2517df57c6e6e
bannerss <roey.kanner@gmail.com>
2022-06-17 18:07:00 +0300

    qwe

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
 app.route('/api/courses').get(get_courses_route_1.getAllCourses);
 app.route('/api/courses/:id').put(save_course_route_1.saveCourse);
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/app/index.html'));
+    res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 const port = (process.env.PORT || 9000);
 const httpServer = app.listen(port, () => {
