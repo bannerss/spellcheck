3356a0968195e3567eb11553a4e2d8efa146ca65
bannerss <roey.kanner@gmail.com>
2022-06-17 09:39:16 +0300

    sadf

--- src/app/courses/directives/highlighted.directive.spec.js
+++
@@ -1,8 +0,0 @@
-import { HighlightedDirective } from './highlighted.directive';
-describe('HighlightedDirective', () => {
-    it('should create an instance', () => {
-        const directive = new HighlightedDirective();
-        expect(directive).toBeTruthy();
-    });
-});
-//# sourceMappingURL=highlighted.directive.spec.js.map
