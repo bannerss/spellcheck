1de5825cec0085579087d6b8fc0f54d918801050
bannerss <roey.kanner@gmail.com>
2022-06-19 17:12:20 +0300

    Sdf

---
+++ server/get-courses.route.d.ts
@@ -0,0 +1,3 @@
+import { Request, Response } from 'express';
+export declare function getAllCourses(req: Request, res: Response): void;
+export declare function getCourseById(req: Request, res: Response): void;
