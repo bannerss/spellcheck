7428b9bb9c0e9dbd242bfd346650a9cd2f55f7a3
bannerss <roey.kanner@gmail.com>
2022-06-20 09:43:37 +0300

    try nx api

--- server/get-courses.route.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from 'express';
-export declare function getAllCourses(req: Request, res: Response): void;
-export declare function getCourseById(req: Request, res: Response): void;
